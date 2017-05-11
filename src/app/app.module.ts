02bfa74ff1d62b7b42e023adcb99681ba5fdf53d
cahuizar <hire@carloshuizar.com>
2017-05-10 20:56:59 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import { provideRoutes} from '@angular/router';
+import {MdCardModule} from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FooterComponent,
+    HomeComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
